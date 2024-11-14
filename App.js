ea10b0497b4cb7df991d688058f247c5d0c657e6
krishnajulakanti <--remove>
2024-11-14 11:20:22 +0530

    init commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>hello</Text>
+      <Text>hello</Text>
+      <Text>hello</Text>
+    </View>
+  );
+};
+
+export default App;
